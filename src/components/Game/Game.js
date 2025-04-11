c2095bd28ee4e58932c4ce11c07832c44d27211d
David Kooymans <david.kooymans@gmail.com>
2025-04-12 05:21:00 +1000

    first 4 tasks done

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-import { sample } from '../../utils';
+import { range, sample } from '../../utils';
 import { WORDS } from '../../data';
+import { checkGuess } from '../../game-helpers'
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants'
+import GuessInput from '../GuessInput'
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +12,50 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  function buildGuesses() {
+    let guesses = [];
+    range(NUM_OF_GUESSES_ALLOWED).forEach(index => {
+      let guess = { row: index, letters: [] }
+      range(5).forEach(charIndex => {
+        guess.letters.push({ char: '', index: charIndex, status: '' })
+      })
+      guesses.push(guess)
+    })
+    return guesses;
+  }
+
+  const [guesses, setGuesses] = React.useState(buildGuesses())
+  const [guessIndex, setGuessIndex] = React.useState(0)
+
+  function addGuess(guess) {
+    if (guessIndex >= NUM_OF_GUESSES_ALLOWED) {
+      window.alert('You have reached 6 guesses')
+      return;
+    }
+    newGuesses = [...guesses]
+    const evalGuess = checkGuess(guess, answer)
+    newGuesses[guessIndex].letters.forEach((letter, index) => {
+      letter.char = evalGuess[index].letter
+      letter.status = evalGuess[index].status
+    })
+    setGuesses(newGuesses)
+    setGuessIndex(guessIndex + 1)
+  }
+
+  return <>
+    <div className="guess-results">
+      {guesses.map((guess, index) => {
+        return <p className="guess" key={index}>
+          {range(5).map(charIndex => {
+            return <span className={`cell ${guess.letters[charIndex].status}`} key={guess.letters[charIndex].index}>{guess.letters[charIndex].char}</span>
+          }
+
+          )}
+        </p>
+      })}
+    </div>
+    <GuessInput addGuess={addGuess} />
+  </>;
 }
 
 export default Game;
---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+function GuessInput({addGuess}) {
+
+  const [guess, setGuess] = React.useState('')
+
+  function submit(event) {
+    event.preventDefault()
+    if(guess.length != 5){
+      window.alert('Must be 5 characters')
+      return;
+    }
+    addGuess(guess);
+    setGuess('');
+  }
+
+  return <form className="guess-input-wrapper" onSubmit={event => submit(event)}>
+    <label htmlFor="guess-input">Enter guess:</label>
+    <input id="guess-input" type="text" value={guess} onChange={event => {
+      let newGuess = event.target.value;
+      if(newGuess.length > 5){
+        return;
+      }
+      
+      setGuess(newGuess.toUpperCase())
+    }} />
+  </form>;
+}
+
+export default GuessInput;
